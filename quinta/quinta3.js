4f4e462685e5f2cf4d4b8fc95288e30a0ab682a0
Antuarte <alinebarbosaantuarte@gmail.com>
2024-10-24 20:54:32 -0300

    Finalização de exercício

--- quinta/quinta3.js
+++ quinta/quinta3.js
@@ -2,27 +2,30 @@ console.log(`7- Em uma competição de saltos ornamentais, 5 (cinco) juízes inf
 de 0 a 10. A nota final do atleta deve excluir a maior e a menor nota dos juízes e é composta pela soma
 das três demais notas. Faça um programa que lê do usuário as cinco notas dos juízes e informa a nota
 final do atleta (a soma das notas excluindo a menor e a maior delas). Utilize uma estrutura de repetição.`)
-
-while (cont < 5){
-    cont = 0
-    cont ++
-    nota = 0
-let nota = Number(prompt("Diga a " + cont + "° nota dos juízes."))
-
-nota += nota
+cont = 0
+nota = 0
+notaAcum = 0
+notaFinal = 0
 maiorNota = 0
 menorNota = Infinity
 
+while (cont < 5){
+    notaFinal = 0
+
+    cont ++
+let nota = Number(prompt("Diga a " + cont + "° nota dos juízes."))
+
+notaAcum += nota
+
 if (nota > maiorNota){
-    maiorNota = nota
+    maiorNota = nota;
 }
 
 if (nota < menorNota){
-    menorNota = nota
+    menorNota = nota;
 }
 
 }
-
-notaFinal = nota - menorNota - maiorNota
+notaFinal = notaAcum - (menorNota + maiorNota)
 notaFinal = notaFinal/3
 alert("A nota final da competição é: " + notaFinal)
